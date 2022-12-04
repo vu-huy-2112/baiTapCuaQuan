9708c4025271479ebc2b26eb15cfa8a108dcf9a9
huyvqit <huyvq.it@gmail.com>
2022-12-04 20:09:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/baitapcuaquan/screen/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.baitapcuaquan.screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.baitapcuaquan.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
